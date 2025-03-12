e9d6e4cf846b635f6d594a4946ae6edfc7be818e
kaka-ubial <karen.ubial@pucpr.edu.br>
2025-03-12 11:45:05 -0300

    landing-page carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./i18n.ts";
-import Header from "./components/Header";
+//import Header from "./components/Header";
+import LandingCarousel from './components/LandingCarousel.tsx';
 
 function App() {
-    return <Header></Header>;
+    return (
+        <>
+            <div>
+                <LandingCarousel />
+            </div>;
+        </>
+    )
 }
 
 export default App;
