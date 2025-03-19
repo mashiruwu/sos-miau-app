f45a22ca83df7393325ff4e1af7e436e9ff24c93
Tagliassi <rafael.galafassi2005@gmail.com>
2025-03-19 19:25:56 -0300

    Terminando front da tela de cadastro e internacionalização da pagina ✅

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 export const DefaultLayout = () => {
+    const location = useLocation();
+
+    // Verifica se a rota atual é "/signup"
+    const isSignupPage = location.pathname === "/Signup";
+
     return (
         <>
             <Header />
-            <main className="mt-16 lg:px-[20%] px-[10%]">
+            <main className={isSignupPage ? "" : "mt-16 lg:px-[20%] px-[10%]"}>
                 <Outlet />
             </main>
             <Footer />
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import SignupImage from "../assets/signup_image.png";
+import { useTranslation } from "react-i18next";
+import SignupImage from "../assets/signup_image2.png";
 
 const Signup = () => {
+    const { t } = useTranslation();
     const [formData, setFormData] = useState({
         name: "",
         surname: "",
@@ -11,6 +13,7 @@ const Signup = () => {
         hasProtectionScreen: "",
         email: "",
         password: "",
+        confirmPassword: "",
         address: "",
         complement: "",
     });
@@ -22,144 +25,174 @@ const Signup = () => {
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            alert(t("signup.password_mismatch"));
+            return;
+        }
         console.log("Form data submitted:", formData);
     };
 
     return (
-        <div className="flex p-10">
-            {/* Formulário */}
-            <div className="bg-white rounded-lg w-full">
-                <form
-                    onSubmit={handleSubmit}
-                    className="flex flex-col space-y-4"
-                >
-                    <h1 className="text-2x1 font-bold mb-2 text-gray-800">
-                        Pronto(a) para ajudar nossos gatinhos?
+        <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-around w-full h-full">
+            {/* Form Section */}
+            <div className="w-full lg:w-2/4 p-4 sm:p-6 lg:p-8">
+                <form onSubmit={handleSubmit} className="p-6">
+                    <h1 className="text-2xl text-[#153151] mb-4 text-center lg:text-left">
+                        {t("signup.title")}
                     </h1>
-                    <p className="text-sm text-gray-500">
-                        Cadastre-se agora para poder adotar os miaus!
+                    <p className="mb-6 text-[#153151] text-center lg:text-left">
+                        {t("signup.description")}
                     </p>
-
-                    <div className="flex space-x-2">
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Nome"
-                            value={formData.name}
-                            onChange={handleChange}
-                            className="border p-2 w-1/2 rounded-md"
-                        />
-                        <input
-                            type="text"
-                            name="surname"
-                            placeholder="Sobrenome"
-                            value={formData.surname}
-                            onChange={handleChange}
-                            className="border p-2 w-1/2 rounded-md"
-                        />
-                    </div>
-
-                    <input
-                        type="text"
-                        name="cpf"
-                        placeholder="CPF"
-                        value={formData.cpf}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <input
-                        type="text"
-                        name="phone"
-                        placeholder="Celular"
-                        value={formData.phone}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <input
-                        type="date"
-                        name="birthdate"
-                        value={formData.birthdate}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <div className="flex items-center space-x-2">
-                        <span className="text-gray-700">Sua residência possui tela de proteção?</span>
-                        <label className="flex items-center space-x-1">
+                    <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-4">
+                        <div className="sm:col-span-2 lg:col-span-1">
+                            <label className="text-[#153151]">{t("signup.name")}</label>
                             <input
-                                type="radio"
-                                name="hasProtectionScreen"
-                                value="Sim"
-                                checked={formData.hasProtectionScreen === "Sim"}
+                                type="text"
+                                name="name"
+                                value={formData.name}
                                 onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
                             />
-                            <span>Sim</span>
-                        </label>
-                        <label className="flex items-center space-x-1">
+                        </div>
+                        <div className="sm:col-span-2 lg:col-span-1">
+                            <label className="text-[#153151]">{t("signup.surname")}</label>
                             <input
-                                type="radio"
-                                name="hasProtectionScreen"
-                                value="Não"
-                                checked={formData.hasProtectionScreen === "Não"}
+                                type="text"
+                                name="surname"
+                                value={formData.surname}
                                 onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
                             />
-                            <span>Não</span>
-                        </label>
+                        </div>
+
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.cpf")}</label>
+                            <input
+                                type="text"
+                                name="cpf"
+                                value={formData.cpf}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.birthdate")}</label>
+                            <input
+                                name="birthdate"
+                                value={formData.birthdate}
+                                onChange={handleChange}
+                                onFocus={(e) => (e.target.type = "date")}
+                                onBlur={(e) => (e.target.type = "text")}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.address")}</label>
+                            <input
+                                type="text"
+                                name="address"
+                                value={formData.address}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.complement")}</label>
+                            <input
+                                type="text"
+                                name="complement"
+                                value={formData.complement}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.phone")}</label>
+                            <input
+                                type="text"
+                                name="phone"
+                                value={formData.phone}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+                        <div className="col-span-2">
+                            <label className="text-[#153151]">{t("signup.email")}</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+
+                        <div className="mt-4 col-span-2">
+                            <span className="block text-[#153151] mb-2">
+                                {t("signup.has_protection_screen")}
+                            </span>
+                            <label className="text-[#153151] mr-4">
+                                <input
+                                    type="radio"
+                                    name="hasProtectionScreen"
+                                    value="Sim"
+                                    checked={formData.hasProtectionScreen === "Sim"}
+                                    onChange={handleChange}
+                                    className="mr-1"
+                                />
+                                {t("signup.yes")}
+                            </label>
+                            <label className="text-[#153151]">
+                                <input
+                                    type="radio"
+                                    name="hasProtectionScreen"
+                                    value="Não"
+                                    checked={formData.hasProtectionScreen === "Não"}
+                                    onChange={handleChange}
+                                    className="mr-1"
+                                />
+                                {t("signup.no")}
+                            </label>
+                        </div>
+
+                        <div>
+                            <label className="text-[#153151]">{t("signup.password")}</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+                        <div>
+                            <label className="text-[#153151]">{t("signup.confirm_password")}</label>
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                className="border-b-2 border-[#153151] w-full p-2"
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="col-span-2 mt-6 w-full bg-[#153151] text-white py-2 rounded-lg hover:bg-[#0f2533] transition-colors duration-300">
+                            {t("signup.submit")}
+                        </button>
                     </div>
-
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Senha"
-                        value={formData.password}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <input
-                        type="text"
-                        name="address"
-                        placeholder="Endereço"
-                        value={formData.address}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <input
-                        type="text"
-                        name="complement"
-                        placeholder="Complemento"
-                        value={formData.complement}
-                        onChange={handleChange}
-                        className="border p-2 rounded-md"
-                    />
-
-                    <button
-                        type="submit"
-                        className="bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition"
-                    >
-                        Cadastrar-se
-                    </button>
                 </form>
             </div>
 
-            {/* Ilustração */}
-            <div className="w-1/2 flex justify-end items-start">
+            {/* Image Section */}
+            <div className="w-full lg:w-1/3 ">
                 <img
                     src={SignupImage}
-                    alt="Ilustração"
-                    className="w-3/4"
+                    alt={t("signup.image_alt")}
+                    className="w-full h-auto"
                 />
             </div>
         </div>
