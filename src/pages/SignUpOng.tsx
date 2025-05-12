44de137d1314d9c5135cfebb1d8b9ed91b3635dd
Diego Henrick <diegohenrick@Diegos-MacBook-Air.local>
2025-05-12 13:24:10 -0300

    wip

---
+++ src/pages/SignUpOng.tsx
@@ -0,0 +1,267 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import SignupImage from "../assets/signup_image2.png";
+import InputField from "../components/InputField/InputField";
+import RadioButton from "../components/RadioButton/RadioButton";
+import Label from "../components/Label/Label";
+import SubmitButton from "../components/SubmitButton/SubmitButton";
+import { useNavigate } from "react-router-dom";
+
+const Signup = () => {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: "",
+        cnpj: "",
+        address: "",
+        phone: "",
+        website: "",
+        foundation_date: "",
+        description: "",
+        socials: [""],
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const [error, setError] = useState({
+        weakpassword: "",
+        passwordmissmatch: "",
+        email: "",
+        cnpj: "",
+        phone: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        setError({
+            weakpassword: "",
+            passwordmissmatch: "",
+            email: "",
+            cnpj: "",
+            phone: "",
+        })
+      
+        if (formData.password !== formData.confirmPassword) {
+          //alert(t("signup.password_mismatch"));
+            setError((prev) => ({
+            ...prev,
+            passwordmissmatch: "senhas não são iguais"
+            }));
+          return;
+        }
+      
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        if (!passwordRegex.test(formData.password)) {
+            //alert(t("signup.weak_password"));
+            setError((prev) => ({
+                ...prev,
+                weakpassword: "senha fraca"
+            }));
+            return;
+        }
+        
+        try {
+          const signupRes = await fetch("http://localhost:3000/donorOng/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+          });
+
+          const result = await signupRes.json();
+
+          if (!signupRes.ok) {
+            console.error("Signup failed:", signupRes.status);
+            setError((prev) => ({
+                ...prev,
+                ...result.errors
+            }));
+            console.log(result.errors)
+            return;
+          }
+      
+          console.log("Signup successful:", result);
+      
+          const loginRes = await fetch("http://localhost:3000/donorOng/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              email: formData.email,
+              password: formData.password,
+            }),
+          });
+      
+          if (!loginRes.ok) {
+            console.error("Login failed:", loginRes.status);
+            return;
+          }
+      
+          const loginData = await loginRes.json();
+          console.log("Login response:", loginData);
+          
+          if (!loginData.token || !loginData.user?.id) {
+            console.error("Resposta de login inválida", loginData);
+            return;
+          }
+      
+          localStorage.setItem("token", loginData.token);
+          sessionStorage.setItem("userId", loginData.user.id);
+          
+          window.location.href = "/";
+        } catch (error) {
+          console.error("Erro no handleSubmit:", error);
+        }
+      };
+      
+      
+
+    return (
+        <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center w-full h-full lg:pb-30">
+            {/* Form Section */}
+            <div className="w-full lg:w-2/4 p-4 sm:p-6 lg:p-8">
+                <form onSubmit={handleSubmit} className="lg:w-1/2 lg:mx-auto">
+                    <h1 className="text-2xl text-[#153151] mb-4 text-center lg:text-left">
+                        {t("signup.title")}
+                    </h1>
+                    <p className="mb-6 text-[#153151] text-center lg:text-left">
+                        {t("signup.description")}
+                    </p>
+                    <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-4">
+                        <div className="sm:col-span-2 lg:col-span-1">
+                            <Label>{t("signup.name")}</Label>
+                            <InputField
+                                type="name"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                placeholder="Digite o nome da ONG"
+                                required
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("signup.cnpj")}</Label>
+                            <InputField
+                                type="text"
+                                name="cnpj"
+                                value={formData.cnpj}
+                                onChange={handleChange}
+                                placeholder="___.___.___-__"
+                                // required
+                            />
+                        <p className={"text-red-400 " + (error.cnpj ? "" : "hidden")}>❗{error.cnpj} </p>
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("signup.address")}</Label>
+                            <InputField
+                                type="text"
+                                name="address"
+                                value={formData.address}
+                                onChange={handleChange}
+                                placeholder="Digite o endereço"
+                                // required
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("signup.phone")}</Label>
+                            <InputField
+                                type="text"
+                                name="phone"
+                                value={formData.phone}
+                                onChange={handleChange}
+                                placeholder="(XX) XXXXX-XXXX"
+                                // required
+                            />
+                            <p className={"text-red-400 " + (error.phone ? "" : "hidden")}>❗{error.phone}</p>
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("signup.email")}</Label>
+                            <InputField
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                placeholder="Digite o email"
+                                required
+                            />
+                            <p className={"text-red-400 " + (error.email ? "" : "hidden")}>❗{error.email}</p>
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("Foundation Date")}</Label>
+                            <InputField
+                                type="text"
+                                name="foundation_date"
+                                value={formData.foundation_date}
+                                onChange={handleChange}
+                                onFocus={(e) => (e.target.type = "date")}
+                                onBlur={(e) => (e.target.type = "text")}
+                                placeholder="__/__/____"
+                                // required
+                            />
+                        </div>
+
+                        <div className="col-span-2">
+                            <Label>{t("signup.description")}</Label>
+                            <InputField
+                                type="text"
+                                name="complement"
+                                value={formData.description}
+                                onChange={handleChange}
+                                placeholder="Fale sobre a ONG"
+                                // required
+                            />
+                        </div>
+
+                        <div>
+                            <Label>{t("signup.password")}</Label>
+                            <InputField
+                                type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                placeholder="Digite sua senha"
+                                required
+                            />
+                            <p className={"text-red-400 " + (error.weakpassword ? "" : "hidden")}>❗{error.weakpassword}</p>
+                        </div>
+                        <div>
+                            <Label>{t("signup.confirm_password")}</Label>
+                            <InputField
+                                type="password"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                placeholder="Confirme sua senha"
+                                required
+                            />
+                            <p className={"text-red-400 " + (error.passwordmissmatch ? "" : "hidden")}>❗{error.passwordmissmatch}</p>
+                        </div>
+                        <SubmitButton>{t("signup.submit")}</SubmitButton>
+                    </div>
+                </form>
+            </div>
+
+            {/* Image Section */}
+            <div className="w-full lg:w-1/3">
+                <img
+                    src={SignupImage}
+                    alt={t("signup.image_alt")}
+                    className="w-full h-auto"
+                />
+            </div>
+        </div>
+    );
+};
+
+
+export default Signup;
