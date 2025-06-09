2f941b9044c0fb184ced1f4fa8a19c721479457f
Helena Carvalho Ferreira <83938491+len4cf@users.noreply.github.com>
2025-06-09 15:37:30 -0300

    Revert "integração gatos com interessados, fix no botao de tenho interesse, o…"

--- src/pages/AdoptedCats.tsx
+++ src/pages/AdoptedCats.tsx
@@ -1,29 +1,30 @@
-import AdoptedCarousel from "../components/AdoptedCatCarousel/AdoptedCarousel";
-import LandingCarousel from "../components/LandingCarousel/LandingCarousel";
+import AdoptedCarousel from "../components/AdoptedCatCarousel/AdoptedCarousel"
+import LandingCarousel from "../components/LandingCarousel/LandingCarousel"
 import { useTranslation } from "react-i18next";
+
 
 const AdoptedCats = () => {
     const { t } = useTranslation();
-
+    
     return (
-        <div>
-            <LandingCarousel />
-            <div className="lg:px-[20%] px-[10%]">
-                <h1 className="font-tiny text-secondary text-3xl md:text-5xl text-center pt-10 md:pt-20">
-                    {t("adopted_cats.title")}
-                </h1>
-                <div className="flex flex-wrap justify-center text-center font-afacad text-lg md:text-xl px-4 md:px-0 pt-6 md:pt-10">
-                    <p className="pb-3 md:pb-5">{t("adopted_cats.text1")}</p>
-                    <p>{t("adopted_cats.text2")}</p>
-                </div>
-                <AdoptedCarousel />
-                <div className="flex flex-wrap justify-center text-center font-afacad text-lg md:text-xl px-4 md:px-0 pb-10 md:pb-20">
-                    <p>{t("adopted_cats.text3")}</p>
-                    <p>{t("adopted_cats.text4")}</p>
-                </div>
+    <div>
+        <LandingCarousel/>
+        <div>       
+            <h1 className="font-tiny text-secondary text-3xl md:text-5xl text-center pt-10 md:pt-20">
+                {t("adopted_cats.title")}
+            </h1>                   
+            <div className="flex flex-wrap justify-center text-center font-afacad text-lg md:text-xl px-4 md:px-0 pt-6 md:pt-10">
+                <p className="pb-3 md:pb-5">{t("adopted_cats.text1")}</p>
+                <p>{t("adopted_cats.text2")}</p>
+            </div>
+            <AdoptedCarousel/>
+            <div className="flex flex-wrap justify-center text-center font-afacad text-lg md:text-xl px-4 md:px-0 pb-10 md:pb-20">
+                <p>{t("adopted_cats.text3")}</p>
+                <p>{t("adopted_cats.text4")}</p>
             </div>
         </div>
-    );
-};
+    </div>
+)
+}
 
-export default AdoptedCats;
+export default AdoptedCats
--- src/types/types.ts
+++ src/types/types.ts
@@ -9,22 +9,8 @@ export interface Gato {
     photo_url?: string;
     description?: string;
     behaviour?: string;
-    neutered?: string;
+    neutered?: boolean;
     fivfelv?: boolean;
     adopted?: string;
     adoption_date?: string | null;
-    interest_count?: number;
-}
-
-export interface Adotante {
-    id?: string;
-    name?: string;
-    surname?: string;
-    email?: string;
-    phone?: string;
-    address?: string;
-    state?: string;
-    cpf?: string;
-    birthday?: string;
-    hasProtectionScreen?: string;
 }
