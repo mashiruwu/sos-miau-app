3a843df834dbe592f5f89250e2f76b56b98ba6df
kaka-ubial <134329528+kaka-ubial@users.noreply.github.com>
2025-06-10 15:33:43 -0300

    integração gatinhos resgatados

--- api/routes/cat.routes.js
+++ api/routes/cat.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const catController = require('../controllers/catController');
 
 router.post('/', catController.createCat);
+router.get('/rescueCats', catController.getRescueCats);
 router.get('/:id', catController.getCat);
 router.put('/:id', catController.updateCat);
 router.delete('/:id', catController.deleteCat);
--- src/pages/Rescue.tsx
+++ src/pages/Rescue.tsx
@@ -2,72 +2,53 @@ import { InfoCarousel } from "../components/InfoCarousel/InfoCarousel";
 import RescueCat from "../assets/rescue_cat.png";
 import Polenta from "../assets/polenta.png";
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { Gato } from "../types/types";
+import gatinho from "../assets/gatinho.jpg";
 
 const Rescue = () => {
+    console.log("Componente Rescue renderizado!");
     const { t } = useTranslation();
 
-    const infoSlides = [
-        {
-            id: 1,
-            title: "Luna",
-            image: RescueCat,
-            content: (
-                <>
-                    <p>{t("rescue.slides.luna")}</p>
-                </>
-            ),
-        },
-        {
-            id: 2,
-            title: "Polenta",
-            image: Polenta,
-            content: (
-                <>
-                    <p>{t("rescue.slides.polenta")}</p>
-                </>
-            ),
-        },
-        {
-            id: 3,
-            title: "Ragnar",
-            image: RescueCat,
-            content: (
-                <>
-                    <p>{t("rescue.slides.ragnar")}</p>
-                </>
-            ),
-        },
-        {
-            id: 4,
-            title: "Billy",
-            image: RescueCat,
-            content: (
-                <>
-                    <p>{t("rescue.slides.billy")}</p>
-                </>
-            ),
-        },
-        {
-            id: 5,
-            title: "Lily",
-            image: RescueCat,
-            content: (
-                <>
-                    <p>{t("rescue.slides.lily")}</p>
-                </>
-            ),
-        },
-        {
-            id: 6,
-            title: "Amora",
-            image: RescueCat,
-            content: (
-                <>
-                    <p>{t("rescue.slides.amora")}</p>
-                </>
-            ),
-        },
-    ];
+    const [gatos, setGatos] = useState<Gato[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect (() => {
+        console.log("useEffect do Rescue disparado!");
+        const fetchCats = async () => {
+            try{
+                const API = import.meta.env.VITE_API_URL;
+                const response = await fetch(
+                    API + "/cat/rescueCats",
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    }
+                );
+ 
+                if (!response.ok)
+                    throw new Error("Erro ao buscar gatos resgatados");
+
+                const data = await response.json();
+                setGatos(data.response);
+            } catch (error) {
+                console.error("Erro ao carregar gatos resgatados:", error);
+                console.error("Erro ao carregar gatos resgatados");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchCats();
+    }, []);
+
+    const infoSlides = gatos.map((gato) => ({
+        title: gato.name,
+        content: gato.description,
+        image: gato.photo_url || gatinho 
+    }));
 
     return (
     <section className="mb-30 flex flex-col gap-10 px-4 md:px-0">
@@ -78,7 +59,7 @@ const Rescue = () => {
         <h1 className="text-center text-2xl md:text-5xl font-tiny uppercase text-secondary">
             {t("rescue.title")}
         </h1>
-        <InfoCarousel slides={infoSlides} />
+        { !loading && <InfoCarousel slides={infoSlides} /> }
     </section>
     );
 };
